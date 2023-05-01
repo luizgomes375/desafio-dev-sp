e9e94c7b250a2c2889c620ff79bd0504d9bc2f84
LuizGomes <luizgomes375@gmail.com>
2023-05-01 13:26:32 -0300

    Commit

--- 5.py
+++ 5.py
@@ -1,7 +1,9 @@
+# Pega uma string de entrada do usuário
 string = input("Digite uma string: ")
+#Seta variável inverted_string como vazio
 inverted_string = ""
-
+#Cria um laço que percorre toda a sting que o usuário inputou de trás pra frente e inverte os campos
 for i in range(len(string)-1, -1, -1):
     inverted_string += string[i]
-
+#imprime a string invertida
 print("A string invertida é:", inverted_string)
